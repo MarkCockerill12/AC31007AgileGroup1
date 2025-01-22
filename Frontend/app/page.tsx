fcad41efef1b43edac8b8d7ec12db928a7161d6a
Hazo <thekinghazo@gmail.com>
2025-01-22 21:07:47 +0000

    Changed around the UI a lot, added some assets

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -9,7 +9,7 @@
 import "./globals.css"
 import Link from "next/link"
 // Import react, usestate, motion for managing functional components and animations
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { motion } from "framer-motion"
 // Import various icons from lucide-react for the UI
 import { Code, Palette, Gamepad, Lightbulb } from "lucide-react"
@@ -50,42 +50,45 @@ export default function App() {
 function Home({ onButtonClick }) {
   return (
     // Display the main text, subtext, and button of the home component all with smooth animations
-    <div className="text-center">
-      <motion.h1
-        className="mainText text-center text-4xl md:text-6xl font-bold mb-4 font-press-start-2p"
-        initial={{ opacity: 0, y: -20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}
-      >
-        NCR ATM
-      </motion.h1>
-      <motion.h2
-        className="text-center text-2xl md:text-3xl mb-8 text-blue-500 dark:text-blue-400"
-        initial={{ opacity: 0, y: -20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5, delay: 0.2 }}
-      >
-        Glory to the New California Republic
-      </motion.h2>
+    <div className="flex flex-col items-center justify-center min-h-[calc(100vh-4rem)]">
+      <div className="text-center">
+        <motion.h1
+          className="mainText text-4xl md:text-6xl font-bold mb-4"
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          NCR ATM
+        </motion.h1>
+        <motion.h2
+          className="text-2xl md:text-3xl mb-8 text-blue-500 dark:text-blue-400"
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+        >
+          Glory to the New California Republic
+        </motion.h2>
 
-      <motion.p
-        className="text-center text-xl mb-8 max-w-2xl mx-auto text-white"
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5, delay: 0.6 }}
-      >
-        To Start Entering Your Transaction, Click Below
-      </motion.p>
-
-      <motion.button
-        className="animate-ping text-center text-xl mt-4 px-4 py-2 text-white font-bold rounded"
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5, delay: 0.6 }}
-        onClick={onButtonClick}
-      >
-        Enter PIN
-      </motion.button>
+        <motion.p
+          className="text-xl mb-8 max-w-2xl mx-auto text-white"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.6 }}
+        >
+          To Start Entering Your Transaction, Click Below
+        </motion.p>
+      </div>
+      <div className="flex justify-center mt-10 cursor-pointer">
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, transitionDelay: 0.6 }}
+          onClick={onButtonClick}
+          style={{ backgroundImage: 'url(/assets/enterCard.png)', backgroundSize: 'contain', backgroundPosition: 'center', backgroundRepeat: 'no-repeat', width: '300px', height: '200px' }}
+          whileHover={{ backgroundImage: 'url(/assets/enterCardHover.png)' }}
+        >
+        </motion.div>
+      </div>
     </div>
   )
 }
@@ -148,45 +151,60 @@ function NumericInput({ setShowSummary, setAccountNumber, setPinNumber }: Numeri
 
   //Return the main div of the numeric input component (our keypad) with various stylings and animations + Back Button
   return (
-    <motion.div
-      className="flex flex-col items-center"
-      initial={{ opacity: 0, y: 20 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5 }}
-    >
-      <label htmlFor="numeric-field" className="text-white font-bold mb-2">
-        {isEnteringPin ? "Enter your PIN" : "Enter your Account Number"}
-      </label>
-      <input
-        type="text"
-        value={isEnteringPin ? pinNumber : accountNumber}
-        readOnly
-        className="border p-2 mb-4 border-gray-300 text-black"
-        id="numeric-field"
-        aria-live="polite"
-      />
-      <div className="grid grid-cols-3 gap-2 mt-2 rounded-md" role="group" aria-label="Numeric keypad">
-        {["1", "2", "3", "4", "5", "6", "7", "8", "9", "Back", "0", "Enter"].map((button) => (
-          <button
-            key={button}
-            className="text-white font-extrabold p-1 text-center transition-transform size-16 duration-200 hover:scale-150"
-            onMouseDown={(e) => e.preventDefault()}
-            onClick={() => handleKeypadClick(button)}
-            aria-label={button}
-          >
-            {button}
-          </button>
-        ))}
-      </div>
-      <button
-        className="mt-4 px-4 py-2 text-white font-bold rounded fixed bottom-0 right-0 mb-4 ml-4"
-        onClick={handleGoBack}
-        aria-label="Go back to main screen"
+    <>
+      <motion.div
+        className="flex flex-col items-center"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
       >
-        Go Back
-      </button>
-    </motion.div>
-  )
+        <label htmlFor="numeric-field" className="text-white font-bold mb-2">
+          {isEnteringPin ? "Enter your PIN" : "Enter your Account Number"}
+        </label>
+        <input
+          type="text"
+          value={isEnteringPin ? pinNumber : accountNumber}
+          readOnly
+          className="border p-2 mb-4 border-gray-300 text-black"
+          id="numeric-field"
+          aria-live="polite"
+        />
+        <div className="grid grid-cols-3 gap-2 mt-2 rounded-md" role="group" aria-label="Numeric keypad">
+          {["1", "2", "3", "4", "5", "6", "7", "8", "9", "Back", "0", "Enter"].map((button) => (
+            <button
+              key={button}
+              className="text-white font-extrabold p-1 text-center transition-transform size-16 duration-200 hover:scale-150"
+              onMouseDown={(e) => e.preventDefault()}
+              onClick={() => handleKeypadClick(button)}
+              aria-label={button}
+            >
+              {button}
+            </button>
+          ))}
+        </div>
+      </motion.div>
+      <div className="fixed top-0 left-0 mt-4 ml-4 flex items-center">
+        <img
+          src="/assets/backButton.png"
+          alt="Back Icon"
+          className="w-6 h-6 cursor-pointer"
+          onClick={handleGoBack}
+        />
+        <motion.button
+          className="px-4 py-2 text-white font-bold rounded transition-transform duration-200 hover:scale-150"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
+          onClick={handleGoBack}
+        >
+          Go Back
+        </motion.button>
+      </div>
+      <div className="fixed top-0 right-0 mt-4 mr-4 text-white mainText text-4xl font-bold mb-4">
+        NCR
+      </div>
+    </>
+  );
 }
   
 // Define the function that displays the "summary" of the account number and pin number for testing
@@ -226,32 +244,46 @@ function Summary({ accountNumber, pinNumber, balance, setBalance, setShowSummary
   }
   // Display to the user their account summary alongside the withdraw/deposit buttons, and the sign out button
   return (
-    <motion.div
-      className="text-center"
-      initial={{ opacity: 0, y: 20 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5 }}
-    >
-      <h2 className="text-white text-2xl mb-4">Account Summary</h2>
-      <p className="text-white mb-2">Account Number: {accountNumber}</p>
-      <p className="text-white mb-2">Balance: £{balance}</p>
-      <p className="text-white mb-2">Last Transaction: £{transactionAmount}</p>
-
-      <button className="mt-4 m-2 px-4 py-2 bg-white text-black rounded" onClick={() => setAction("withdraw")}>
-        Withdraw
-      </button>
-      <button className="mt-4 m-2 px-4 py-2 bg-white text-black rounded" onClick={() => setAction("deposit")}>
-        Deposit
-      </button>
-      <button
-        className="mt-4 px-4 py-2 text-white font-bold rounded fixed bottom-0 right-0 mb-4 ml-4"
-        onClick={handleGoBack}
-        aria-label="Go back to main screen"
+    <>
+      <motion.div
+        className="text-center"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
       >
-        Sign Out
-      </button> 
-    </motion.div>
+        <h2 className="text-white text-2xl mb-4">Account Summary</h2>
+        <p className="text-white mb-2">Account Number: {accountNumber}</p>
+        <p className="text-white mb-2">Balance: £{balance}</p>
+        <p className="text-white mb-2">Last Transaction: £{transactionAmount}</p>
 
+        <button className="mt-4 m-6 px-4 py-2 bg-white text-black rounded transition-transform duration-200 hover:scale-150" onClick={() => setAction("withdraw")}>
+          Withdraw
+        </button>
+        <button className="mt-4 m-6 px-4 py-2 bg-white text-black rounded transition-transform duration-200 hover:scale-150" onClick={() => setAction("deposit")}>
+          Deposit
+        </button>
+      </motion.div>
+      <div className="fixed top-0 left-0 mt-4 ml-4 flex items-center">
+        <img
+          src="/assets/backButton.png"
+          alt="Back Icon"
+          className="w-6 h-6 cursor-pointer"
+          onClick={handleGoBack}
+        />
+        <motion.button
+          className="px-4 py-2 text-white font-bold rounded transition-transform duration-200 hover:scale-150"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
+          onClick={handleGoBack}
+        >
+          Sign Out
+        </motion.button>
+      </div>
+      <div className="fixed top-0 right-0 mt-4 mr-4 text-white mainText text-4xl font-bold mb-4">
+        NCR
+      </div>
+    </>
   )
 }
 
@@ -277,50 +309,67 @@ function Withdraw({ accountNumber, pinNumber, balance, setBalance, setShowSummar
   }
  // Display the withdraw component with the account number, pin number, balance, and withdraw buttons
   return (
-    <motion.div
-      className="text-center"
-      initial={{ opacity: 0, y: 20 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5 }}
-    >
-      <h2 className=" text-white text-2xl mb-4">Withdraw</h2>
-      <p className="text-white mb-2">Account Number: {accountNumber}</p>
-      <p>PIN Number: {pinNumber}</p>
-      <p className="text-white mb-2">Balance: £{balance}</p>
+    <>
+      <motion.div
+        className="text-center"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        <h2 className=" text-white text-2xl mb-4">Withdraw</h2>
+        <p className="text-white mb-2">Account Number: {accountNumber}</p>
+        <p className="text-white mb-2"> PIN Number: {pinNumber}</p>
+        <p className="text-white mb-2">Balance: £{balance}</p>
 
-      <h2 className=" text-white text-2xl mb-4">Withdraw how much?</h2>
+        <h2 className=" text-white text-2xl mb-4">Withdraw how much?</h2>
 
-      {["5", "10", "20", "50", "100"].map((amount) => (
-        <button
-          key={amount}
-          className="mt-4 m-2 px-4 py-2 bg-white text-black rounded"
-          onClick={() => handleWithdraw(Number.parseInt(amount))}
+        <div className="grid grid-cols-2 grid-rows-3 gap-4">
+          {["1", "5", "10", "20", "50", "100"].map((amount) => (
+            <button
+              key={amount}
+              className="mt-4 m-2 px-4 py-2 bg-white text-black rounded"
+              onClick={() => handleWithdraw(Number.parseInt(amount))}
+            >
+              £{amount}
+            </button>
+          ))}
+          <input
+            type="number"
+            value={customAmount}
+            onChange={(e) => setCustomAmount(e.target.value)}
+            className="mt-4 m-2 px-4 py-2 bg-white text-black rounded"
+            placeholder="Custom Amount"
+            min="1"
+          />
+          <button
+            className="mt-4 m-2 px-4 py-2 bg-white text-black rounded"
+            onClick={() => handleWithdraw(Number.parseInt(customAmount))}
+          >
+            Withdraw Custom Amount
+          </button>
+        </div>
+      </motion.div>
+      <div className="fixed top-0 left-0 mt-4 ml-4 flex items-center">
+        <img
+          src="/assets/backButton.png"
+          alt="Back Icon"
+          className="w-6 h-6 cursor-pointer"
+          onClick={handleGoBack}
+        />
+        <motion.button
+          className="px-4 py-2 text-white font-bold rounded transition-transform duration-200 hover:scale-150"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
+          onClick={handleGoBack}
         >
-          £{amount}
-        </button>
-      ))}
-      <input
-        type="number"
-        value={customAmount}
-        onChange={(e) => setCustomAmount(e.target.value)}
-        className="mt-4 m-2 px-4 py-2 bg-white text-black rounded"
-        placeholder="Custom Amount"
-        min="1"
-      />
-      <button
-        className="mt-4 m-2 px-4 py-2 bg-white text-black rounded"
-        onClick={() => handleWithdraw(Number.parseInt(customAmount))}
-      >
-        Withdraw Custom Amount
-      </button>
-
-      <button
-        className="mt-4 px-4 py-2 text-white font-bold rounded fixed bottom-0 right-0 mb-4 ml-4"
-        onClick={handleGoBack}
-      >
-        Go Back
-      </button>
-    </motion.div>
+          Go Back
+        </motion.button>
+      </div>
+      <div className="fixed top-0 right-0 mt-4 mr-4 text-white mainText text-4xl font-bold mb-4">
+        NCR
+      </div>
+    </>
   )
 }
 
@@ -346,6 +395,7 @@ function Deposit({ accountNumber, pinNumber, balance, setBalance, setShowSummary
 
   // Display the deposit component with the account number, pin number, balance, and withdraw buttons
   return (
+    <>
     <motion.div
       className="text-center"
       initial={{ opacity: 0, y: 20 }}
@@ -354,41 +404,57 @@ function Deposit({ accountNumber, pinNumber, balance, setBalance, setShowSummary
     >
       <h2 className=" text-white text-2xl mb-4">Deposit</h2>
       <p className="text-white mb-2">Account Number: {accountNumber}</p>
-      <p>PIN Number: {pinNumber}</p>
+      <p className="text-white mb-2"> PIN Number: {pinNumber}</p>
       <p className="text-white mb-2">Balance: £{balance}</p>
 
       <h2 className=" text-white text-2xl mb-4">Deposit how much?</h2>
 
-      {["5", "10", "20", "50", "100"].map((amount) => (
-        <button
-          key={amount}
+      <div className="grid grid-cols-2 grid-rows-3 gap-4">
+        {["1", "5", "10", "20", "50", "100"].map((amount) => (
+          <button
+            key={amount}
+            className="mt-4 m-2 px-4 py-2 bg-white text-black rounded"
+            onClick={() => handleDeposit(Number.parseInt(amount))}
+          >
+            £{amount}
+          </button>
+        ))}
+        <input
+          type="number"
+          value={customAmount}
+          onChange={(e) => setCustomAmount(e.target.value)}
           className="mt-4 m-2 px-4 py-2 bg-white text-black rounded"
-          onClick={() => handleDeposit(Number.parseInt(amount))}
+          placeholder="Custom Amount"
+          min="1"
+        />
+        <button
+          className="mt-4 m-2 px-4 py-2 bg-white text-black rounded"
+          onClick={() => handleDeposit(Number.parseInt(customAmount))}
         >
-          £{amount}
+          Deposit Custom Amount
         </button>
-      ))}
-      <input
-        type="number"
-        value={customAmount}
-        onChange={(e) => setCustomAmount(e.target.value)}
-        className="mt-4 m-2 px-4 py-2 bg-white text-black rounded"
-        placeholder="Custom Amount"
-        min="1"
-      />
-      <button
-        className="mt-4 m-2 px-4 py-2 bg-white text-black rounded"
-        onClick={() => handleDeposit(Number.parseInt(customAmount))}
-      >
-        Deposit Custom Amount
-      </button>
-
-      <button
-        className="mt-4 px-4 py-2 text-white font-bold rounded fixed bottom-0 right-0 mb-4 ml-4"
-        onClick={handleGoBack}
-      >
-        Go Back
-      </button>
+      </div>
     </motion.div>
+    <div className="fixed top-0 left-0 mt-4 ml-4 flex items-center">
+        <img
+          src="/assets/backButton.png"
+          alt="Back Icon"
+          className="w-6 h-6 cursor-pointer"
+          onClick={handleGoBack}
+        />
+        <motion.button
+          className="px-4 py-2 text-white font-bold rounded transition-transform duration-200 hover:scale-150"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
+          onClick={handleGoBack}
+        >
+          Go Back
+        </motion.button>
+      </div>
+      <div className="fixed top-0 right-0 mt-4 mr-4 text-white mainText text-4xl font-bold mb-4">
+        NCR
+      </div>
+    </>
   )
 }
