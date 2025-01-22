fe518e771c6e4de10ad82aeb2781c9c9513ffeb0
Hazo <thekinghazo@gmail.com>
2025-01-22 21:07:47 +0000

    Changed around the UI a lot, added some assets

--- Frontend/app/layout.tsx
+++ Frontend/app/layout.tsx
@@ -1,5 +1,6 @@
 // Import CSS file
 import './globals.css';
+import React from 'react'
 // Define metadata for the app
 export const metadata = {
   title: 'NCR ATM',
@@ -14,8 +15,16 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={`scroll-smooth`} >
+      <body>
+          <div className="relative min-h-screen bg-gradient-custom">
+            <div className="relative z-10 flex flex-col min-h-screen">
+              <main className="flex-grow container mx-auto px-4 py-8">
+                {children}
+              </main>
+            </div>
+          </div>
+      </body>
     </html>
   )
 }
